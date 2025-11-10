72618e6d27ad0f259b8ac5bb07a3faeef793331a
rajabboyadilbekov2010 <rajabboydev@gmail.com>
2025-11-10 18:25:11 +0300

    homework

---
+++ main;.py
@@ -0,0 +1,26 @@
+# davlatlar =["rassiya","o'zbekistan","karea","hindiston"]
+# davlatlar =["rassiya","o'zbekistan","karea","hindiston"]
+# print(sorted(davlatlar))
+# print(davlatlar)
+#
+#
+# son=[13,24,67,67,24,99,68]
+# son.sort()
+# print(son)
+# son.sort(reverse=True)
+
+
+ismlar =["Suhrob","Abbos","Mirzohid","Rajabboy"]
+ismlar.sort()
+print(ismlar)
+
+juft_sonlar = list(range(120,1200,2))
+print(juft_sonlar)
+juft_sonlar = list(range(120,1200,2))
+jami=sum(juft_sonlar)
+print(jami)
+
+taomlar=["osh","sho'rva","kabob","qovurdoq","so'msa"]
+nonushta=["qaymoq","gamburger","hot-dog"]
+print(taomlar)
+print(nonushta)
---
+++ salom.py
@@ -0,0 +1,5 @@
+gmail =input(" gmail manzil kriting")
+if "@" not in gmail:
+    print("@ begisi yo'q!")
+else:
+    print("gmail qabul qilindi")
