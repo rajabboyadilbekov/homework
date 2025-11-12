cc045d9119f2956b8a0ec38e9a44cab082aae338
rajabboyadilbekov2010 <rajabboydev@gmail.com>
2025-11-12 22:38:54 +0300

    .

---
+++ homework9.py
@@ -0,0 +1,48 @@
+#1
+# ism=input("ismingiz nima?>>>")
+# yosh=input("yoshingiz nechada?>>>")
+# print(f"{ism} {2025-float(yosh)}yilda tug'ilgan ekansiz")
+# #2
+# son=input("istalgan son kriting>>>")
+# print(float(son)**2)
+# print(float(son)**3)
+#3
+# toq= list(range(1,9999999,2))
+# juft=list(range(0,9999999,2))
+# son =input("istalgan son kriting>>> ")
+# if "son" not in juft:
+#     print("siz kritgan son toq son")
+# else:
+#     print("siz kritgan son juft")
+#4
+# son =input("istalgan 1-sonni kriting>>> ")
+# son2 =input("istalgan 2-sonni kriting>>> ")
+# if"son"<son2:
+#     print("kattta son =",son)
+# else:
+#     print("katta son =",son2)
+#5
+# x=float(input("x="))
+# y=float(input("y="))
+# print(x*x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
